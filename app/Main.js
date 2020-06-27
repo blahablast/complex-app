a2f872120014a433bb295f34959418793afa9bc6
blahablast <37739821+blahablast@users.noreply.github.com>
2020-06-27 17:13:25 -0400

    initial commit

---
+++ app/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+const Main = () => {
+  return (
+    <div>
+      <h1>This is our app</h1>
+      <p>Cheers</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<Main />, document.getElementById("app"))
