5558c9e7a4ae3212f8d970725a2738d3d606c211
blahablast <37739821+blahablast@users.noreply.github.com>
2020-06-28 21:28:20 -0400

    added a bunch of components

--- app/Main.js
+++ app/Main.js
@@ -1,12 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
+import Header from "./components/Header"
+import HomeGuest from "./components/HomeGuest"
+import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
 const Main = () => {
   return (
-    <div>
-      <h1>This is our app</h1>
-      <p>Cheers</p>
-    </div>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGuest />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
   )
 }
 
